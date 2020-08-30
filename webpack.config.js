3520fe0a8b0ceff3d61ad115400bf6441e524cc9
Docterryome Wilkins <docterryomewilkins@DocterryomesAir.attlocal.net>
2020-08-30 09:54:56 -0500

    add webpack stuff

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/client/js/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
@@ -22,5 +22,4 @@ module.exports = {
             }
         ]
     }
-
 }
