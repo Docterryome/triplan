0ba7915dbeae020036848cecc9ed0f449180eb4e
Docterryome Wilkins <docterryomewilkins@DocterryomesAir.attlocal.net>
2020-08-31 16:53:14 -0500

    added express things

--- src/server/server.js
+++ src/server/server.js
@@ -1,3 +1,10 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static("dist"));
+app.listen(8080, () => console.log("Running on Port 8080"));
