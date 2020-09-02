096461bf2af4cf66bea95b1c8118531cd2efdd84
Docterryome Wilkins <docterryomewilkins@DocterryomesAir.attlocal.net>
2020-09-01 20:13:08 -0500

    added more stuff

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,20 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
+    mode: 'development',
     entry: './src/client/js/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
     },
+    devtools: 'inline-source-map',
+    devserver: {
+        contentBase: './dist'
+    },
     plugins: [
-        new CleanWebpackPlugin(),
+        new CleanWebpackPlugin({cleanStaleWebpackAssets: true}),
         new HtmlWebpackPlugin({
-            title: 'Output Managment'
+            title: 'Triplin'
         })
     ],
     module: {
