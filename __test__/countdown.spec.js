756615cb35619ab8124185d8e5d7c0477c4d5160
Docterryome Wilkins <docterryomewilkins@DocterryomesAir.attlocal.net>
2020-09-03 20:50:41 -0500

    Fixed test cases

--- __test__/countdown.spec.js
+++ __test__/countdown.spec.js
@@ -1,9 +1,8 @@
 import { parseDate, getDays } from '../src/client/js/countdown.js';
 
-testSuite("Testing out parse Date", () => { 
 
-    test("Test parse date with 2020-09-01", () => {
-        const myDate = parseDate("2020-09-01");
-        expect(myDate).toBe(new Date("2020", "09", "01"));
-    });
-})
+
+test("Test parse date with 2020-09-01", () => {
+    const myDate = parseDate("2020-09-01");
+    expect(myDate).toStrictEqual(new Date("2020", "09", "01"));
+});
