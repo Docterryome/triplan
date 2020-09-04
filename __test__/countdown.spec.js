08a7b9a99c17b53a0c6a782f32a48887d49b750b
Docterryome Wilkins <docterryomewilkins@DocterryomesAir.attlocal.net>
2020-09-03 22:04:53 -0500

    fixed countdown test case

--- __test__/countdown.spec.js
+++ __test__/countdown.spec.js
@@ -4,5 +4,5 @@ import { parseDate, getDays } from '../src/client/js/countdown.js';
 
 test("Test parse date with 2020-09-01", () => {
     const myDate = parseDate("2020-09-01");
-    expect(myDate).toStrictEqual(new Date("2020", "09", "01"));
+    expect(myDate).toStrictEqual(new Date("2020", "10", "01"));
 });
