8acd601f467b8e60c6333a750b8ae3abccbf1692
Docterryome Wilkins <docterryomewilkins@DocterryomesAir.attlocal.net>
2020-09-06 13:41:14 -0500

    Add over 16 days to the issue

--- src/client/js/weather.js
+++ src/client/js/weather.js
@@ -1,6 +1,6 @@
 //Import countdown Libraries
 import {getDays} from './countdown';
-import { initial } from 'lodash';
+import { response } from 'express';
 
 //WeatherBit api key
 const weatherBitAPI = "http://api.weatherbit.io/v2.0";
@@ -10,30 +10,45 @@ const LOCALSERVER = "http://localhost:3000";
 
 export const getWeatherData = async (cityData, date) => {
     let response;
+    let weatherURL;
     const daysAhead = getDays(date, Date.now()) + 1;
     //If Date is greater than 16 days get historical date to predict temp
     const dtFormat = new Intl.DateTimeFormat("en", {year: "numeric", mount: "2-digit", day: "2-digit"});
     console.log("Days Ahead: " + daysAhead);
     if( daysAhead < 16){
-        let weatherURL = { url: `${weatherBitAPI}/forecast/daily?days=${daysAhead}&lat=${cityData.lat}&lon=${cityData.lng}&units=I&key=${API_KEY}`};
+        weatherURL = { url: `${weatherBitAPI}/forecast/daily?days=${daysAhead}&lat=${cityData.lat}&lon=${cityData.lng}&units=I&key=${API_KEY}`};
         console.log(JSON.stringify(weatherURL));
-        response = await fetch(`${LOCALSERVER}/getWeather`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(weatherURL)
-        });
+        response = postData(`${LOCALSERVER}/getWeather`, weatherURL);
         return response.json();
     }
     //Get 16 day forcast
     else{
-        return "";
+        const startDate = parseEnDateByYear(dtFormat.format(date), 1);
+        const endDate = parseDateAddDays(startDate, 1);
+        weatherURL = {url: `${weatherBitAPI}/history/daily&lat=${cityData.lat}&start_date=${startDate}&end_date=${endDate}&lon=${cityData.lng}&units=I&key=${API_KEY}`};
+        response = await postData(`${LOCALSERVER}/getWeather`, weatherURL);
+        return response.json();
     }
 }
 
 
-const parseEnDate = (str) => {
+export const parseEnDateByYear = (str, year) => {
     const datef = str.split('/');
-    return `${datef[2]}-${datef[0]}-${datef[1]}`
+    return `${parseInt(datef[2]) - year}-${datef[0]}-${datef[1]}`
+}
+
+export const parseDateAddDays = (str, days) => {
+    const datef = str.split("-");
+    return `${datef[0]}-${datef[1]}-${parseInt(datef[2]) + days}`
+}
+
+export const postData = async (url, body) => {
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    });
+    return response;
 }
