e80fa986eed25acab916dfc0e2ed6bbfa490cf49
Docterryome Wilkins <docterryomewilkins@DocterryomesAir.attlocal.net>
2020-09-11 13:40:59 -0500

    Adding stuff to make this production

--- __test__/geoNamesAPI.spec.js
+++ __test__/geoNamesAPI.spec.js
@@ -1,1 +1,7 @@
 import {getGeoLocation, getLatitudeLongitude } from '../src/client/js/geoNames';
+
+test("Get name of city",  () => {
+    getGeoLocation("New York").then(data => {
+        expect(data.geonames[0].toponymName).toBe("New York City");
+    });
+});
