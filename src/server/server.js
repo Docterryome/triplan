070833a037ee4286cb869dfcda28f2f51ce18129
Docterryome Wilkins <docterryomewilkins@DocterryomesAir.attlocal.net>
2020-09-11 11:54:44 -0500

    Changed port

--- src/server/server.js
+++ src/server/server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.static("dist"));
-app.listen(3000, () => console.log("Running on Port 3000"));
+app.listen(process.env.PORT || 3000, () => console.log("Running on Port 3000"));
 
 async function getWeather(request, response) {
     let weatherObj = await getJson(request.body.url);
