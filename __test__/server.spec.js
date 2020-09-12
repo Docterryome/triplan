e7c8aed596da7ff8ed7f97a4960454247848c29f
Docterryome Wilkins <docterryomewilkins@DocterryomesAir.attlocal.net>
2020-09-12 14:35:27 -0500

    Removed api keys

--- __test__/server.spec.js
+++ __test__/server.spec.js
@@ -3,9 +3,8 @@ const { json } = require('body-parser');
 const request = require('supertest');
 
 test("Testing the Weather Request", async () => {
-
-    const weatherUrl = { url: 'https://api.weatherbit.io/v2.0/forecast/daily?days=16&units=I&lat=18.47116&lon=-77.91883&key=09959aeb527c4c0589b4fc648879cd70'};
-
+    const API_KEY = process.env.WEATHERBIT_KEY;
+    const weatherUrl = { url: `https://api.weatherbit.io/v2.0/forecast/daily?days=16&units=I&lat=18.47116&lon=-77.91883&key=${API_KEY}`};
     const response = await request(app).post("/getWeather").send(weatherUrl);
     expect(response.statusCode).toBe(200);
 });
